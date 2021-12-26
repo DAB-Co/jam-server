8f6c3be5032a3b45300be77537d5adf5dce9b38a
atillaturkmen <turkmenatilla522@gmail.com>
2021-12-26 11:19:05 +0300

    add https (#11)

--- jam_server_main.js
+++ jam_server_main.js
@@ -2,6 +2,16 @@ const express = require("express"); // framework for nodejs
 const path = require("path"); // useful for joining file paths
 const body_parser = require("body-parser");
 require("dotenv").config({ path: path.join(__dirname, ".env.local") });
+// read command line arguments
+const argv = require("yargs")(process.argv.slice(1))
+    .option("no_https", {
+        description: "runs http only",
+        alias: "n",
+        type: "boolean",
+        default: false,
+    })
+    .help().alias("help", "h")
+    .parse();
 
 const app = express(); // create express app
 
@@ -11,10 +21,23 @@ app.use(express.urlencoded({
 }));
 app.use(body_parser.json());
 
-// Start listening to given port
-const server = app.listen(process.env.port, () => {
-    console.log(`Running on port ${server.address().port}`);
-});
+if (argv.no_https) {
+    // Start listening to http port
+    const http_server = app.listen(process.env.http_port, () => {
+        console.log(`Running on port ${http_server.address().port}`);
+    });
+} else {
+    // Start listening to https port
+    const https = require('https');
+    const https_port = process.env.https_port;
+    const credentials = {
+        key: fs.readFileSync(process.env.private_key_dir, 'utf8'),
+        cert: fs.readFileSync(process.env.certificate_dir, 'utf8'),
+    };
+    const https_server = https.createServer(credentials, app).listen(https_port, function () {
+        console.log(`Running on port ${https_server.address().port}`);
+    });
+}
 
 // Let routes folder handle every get and post request
 const routes = require(path.join(__dirname, "routes", "index")); // look at index.js file in routes folder
