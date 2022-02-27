4f7afd8aa633c852635aa71d7a42f72c56dcdfab
dbc201 <denizbcakiroglu@gmail.com>
2022-02-26 19:19:41 -0500

    add async await

--- utils/algorithmEntryPoint.js
+++ utils/algorithmEntryPoint.js
@@ -26,10 +26,10 @@ function add_preference(user_id, raw_preference) {
         const type = item.type;
         const id = item.id;
         const existing_data = userPreferencesUtils.getPreference(user_id, type, id);
+        let users_to_update = userPreferencesUtils.getCommonUserIds(type, id);
         let weight_to_be_added = (i + 1)*type_weights[type];
         if (existing_data === undefined) {
             userPreferencesUtils.addPreference(user_id, type, id, weight_to_be_added);
-            let users_to_update = userPreferencesUtils.getCommonUserIds(type, id);
             for (let i = 0; i < users_to_update.length; i++) {
                 if (users_to_update[i] !== user_id) {
                     let old_weight = userConnectionsUtils.getWeight(users_to_update[i], user_id);
@@ -169,10 +169,10 @@ class AlgorithmEntryPoint {
         add_preference(user_id, raw_tracks);
     }
 
-    run() {
+    async run() {
         let users = spotifyUtils.getAllPrimaryKeys();
         for (let i=0; i<users.length; i++) {
-            this.updatePreferences(users[i]);
+            await this.updatePreferences(users[i]);
         }
     }
 }
