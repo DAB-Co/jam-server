e3c3b160fc1115f2836733c1f3d458714ea8cf93
dbc201 <denizbcakiroglu@gmail.com>
2022-02-28 20:47:48 -0500

    add unit_tests

---
+++ test/languages_test.js
@@ -0,0 +1,117 @@
+const axios = require('axios');
+const assert = require("assert");
+
+const path = require("path");
+require("dotenv").config({path: path.join(__dirname, "..", ".env.local")});
+const domain = "http://localhost:" + process.env.http_port;
+
+const Database = require("@dab-co/jam-sqlite").Database;
+const UtilsInitializer = require("@dab-co/jam-sqlite").Utils.UtilsInitializer;
+
+describe(__filename, function () {
+    let database = undefined;
+    let utilsInitializer = undefined;
+    let b_id = undefined;
+    before(function() {
+        database = new Database(path.join(__dirname, "..", "sqlite", "database.db"));
+        utilsInitializer = new UtilsInitializer(database);
+        b_id = utilsInitializer.accountUtils().addUser("beethoven@email.com", "beethoven", "ilovethe9th", "inout").lastInserRowId;
+        console.log(b_id);
+    });
+
+    describe("", function() {
+        it("beethoven adds language german", async function () {
+            let data = {
+                user_id: b_id,
+                api_token: "inout",
+                add_languages: ["ger"],
+                remove_languages: [],
+            }
+            await axios.post(domain+"/api/update_languages", data)
+                .then(function (res){
+                    assert.strictEqual(res.status, 200);
+                    assert.strictEqual(res.data, "OK");
+                })
+                .catch(function (err) {
+                    assert.fail(err.response.data);
+                });
+        });
+    });
+
+    describe("", function() {
+       it("beethoven adds languages french, english and polish", async function () {
+           let data = {
+               user_id: b_id,
+               api_token: "inout",
+               add_languages: ["fr", "en", "pol"],
+               remove_languages: [],
+           }
+           await axios.post(domain+"/api/update_languages", data)
+               .then(function (res){
+                   assert.strictEqual(res.status, 200);
+                   assert.strictEqual(res.data, "OK");
+               })
+               .catch(function (err) {
+                   assert.fail(err.response.data);
+               });
+       });
+    });
+
+    describe("", function() {
+       it("beethoven adds arabic and persian and removes french and english", async function () {
+           let data = {
+               user_id: b_id,
+               api_token: "inout",
+               add_languages: ["ar", "per"],
+               remove_languages: ["en"],
+           }
+           await axios.post(domain+"/api/update_languages", data)
+               .then(function (res){
+                   assert.strictEqual(res.status, 200);
+                   assert.strictEqual(res.data, "OK");
+               })
+               .catch(function (err) {
+                   assert.fail(err.response.data);
+               });
+       });
+    });
+
+    describe("", function () {
+       it("beethoven removes polish", async function () {
+           let data = {
+               user_id: b_id,
+               api_token: "inout",
+               add_languages: [],
+               remove_languages: ["pol"],
+           }
+           await axios.post(domain+"/api/update_languages", data)
+               .then(function (res){
+                   assert.strictEqual(res.status, 200);
+                   assert.strictEqual(res.data, "OK");
+               })
+               .catch(function (err) {
+                   assert.fail(err.response.data);
+               });
+       });
+    });
+
+    describe("", function () {
+        it("beethoven removes arabic and persian", async function () {
+            let data = {
+                user_id: b_id,
+                api_token: "inout",
+                add_languages: [],
+                remove_languages: ["ar", "per"],
+            }
+            await axios.post(domain+"/api/update_languages", data)
+                .then(function (res){
+                    assert.strictEqual(res.status, 200);
+                    assert.strictEqual(res.data, "OK");
+                })
+                .catch(function (err) {
+                    assert.fail(err.response.data);
+                });
+        });
+    });
+});
+
