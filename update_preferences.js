41fc0ecd70e8487a8324c8a3f4a5dadcdb53711f
sysadmin <sysadmin@omniamorsaequat.com>
2022-03-29 18:49:20 +0000

    udpate_perferend

--- update_preferences.js
+++ update_preferences.js
@@ -1,3 +1,14 @@
 const path = require("path");
 const algorithm = require(path.join(__dirname, "utils", "algorithmEntryPoint.js"));
-algorithm.updatePreferences().then();
+const utilsInitializer = require(path.join(__dirname, "utils", "initializeUtils.js"));
+
+const users = utilsInitializer.accountUtils().getAllPrimaryKeys();
+
+async function update_users() {
+	for (let i=0; i<users.lengh; i++){
+		await algorithm.updatePreferences(users[i]);
+	}
+}
+
+update_users().then();
+
