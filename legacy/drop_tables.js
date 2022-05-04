361f95fe8cd58d4a2c1d07c21f405d8adaeedea8
dbc201 <denizbcakiroglu@gmail.com>
2022-05-04 09:36:30 -0400

    comment unused legacy drop

--- legacy/drop_tables.js
+++ legacy/drop_tables.js
@@ -5,7 +5,7 @@ const database = utilsInitializer.accountUtils().databaseWrapper.database;
 database.exec("DROP TRIGGER IF EXISTS before_user_connections_insert");
 database.exec("DROP TRIGGER IF EXISTS before_user_friends_insert");
 database.exec("DROP TRIGGER IF EXISTS before_user_languages_insert");
-database.exec("DROP TRIGGER IF EXISTS before_user_preferences_insert");
+//database.exec("DROP TRIGGER IF EXISTS before_user_preferences_insert");
 database.exec("DROP TRIGGER IF EXISTS insert_Timestamp_Trigger");
 database.exec("DROP TRIGGER IF EXISTS update_Timestamp_Trigger");
 
